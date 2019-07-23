f05d137f6a571db946150e75d7ebfff2ce3cda0a
juanmi <juancabrera.dev@gmail.com>
2019-07-23 19:12:06 +0200

    Init project

---
+++ src/main/java/com/ryanair/jmcr/InterconnectingFlightsApplication.java
@@ -0,0 +1,13 @@
+package com.ryanair.jmcr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterconnectingFlightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterconnectingFlightsApplication.class, args);
+	}
+
+}
