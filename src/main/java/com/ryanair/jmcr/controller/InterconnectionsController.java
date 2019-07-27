b587d6a3416bcc3e67453b67c01dc8c4ba898b35
juanmi <juancabrera.dev@gmail.com>
2019-07-27 13:16:48 +0200

    Refactor controller

--- src/main/java/com/ryanair/jmcr/controller/InterconnectionsController.java
+++ src/main/java/com/ryanair/jmcr/controller/InterconnectionsController.java
@@ -12,9 +12,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ryanair.jmcr.controller.dto.FlightSearch;
-import com.ryanair.jmcr.service.Consumer;
-import com.ryanair.jmcr.service.routes.dto.RouteAPI; 
-
+import com.ryanair.jmcr.service.Converter;
+import com.ryanair.jmcr.service.dto.Flight;
+import com.ryanair.jmcr.service.routes.IRoutesConsumer;
+import com.ryanair.jmcr.service.routes.IRoutesService;
+import com.ryanair.jmcr.service.routes.dto.RouteAPI;
+import com.ryanair.jmcr.service.routes.dto.RouteSearch;
+import com.ryanair.jmcr.service.schedules.ISchedulesConsumer;
+import com.ryanair.jmcr.service.schedules.ISchedulesService;
+import com.ryanair.jmcr.service.schedules.dto.ScheduleAPI; 
 
 @RestController
 @RequestMapping("seeker")
@@ -22,16 +28,19 @@ import com.ryanair.jmcr.service.routes.dto.RouteAPI;
 public class InterconnectionsController {
 
 	@Autowired
-	Consumer<RouteAPI> routesConsumer;
-	
-//	@Autowired
-//	RoutesService routesService;
-//	
-//	@Autowired
-//	SchedulesConsumer schedulesConsumer;
-//	
-//	@Autowired
-//	SchedulesService schedulesService;
+	Converter<RouteSearch, FlightSearch> searchConverter;
+
+	@Autowired
+	IRoutesConsumer routesConsumer;
+
+	@Autowired
+	IRoutesService routesService;
+
+	@Autowired
+	ISchedulesConsumer schedulesConsumer;
+
+	@Autowired
+	ISchedulesService schedulesService;
 
 	@GetMapping(value = "/interconnections")
 	public ResponseEntity<Object> findFlights(
@@ -39,29 +48,39 @@ public class InterconnectionsController {
 			@RequestParam(value="arrival") String arrival,
 			@RequestParam(value="departureDateTime") String departureDateTime,
 			@RequestParam(value="arrivalDateTime") String arrivalDateTime) {
-		
+
 		try {
-			FlightSearch search = new FlightSearch(departure, arrival, departureDateTime, arrivalDateTime);
-			
-			/**
-			 * 1. Collect Routes
-			 * 2. Map-Reduce Routes
-			 * 3. Collect Schedules
-			 * 4. Map-Reduce Schedules
-			 */
-			List<RouteAPI> apiRoutes = routesConsumer.collect();
-//			List<Route> routes = routesService.filter(apiRoutes, search);
-//			List<ScheduleAPI> apiSchedules = schedulesConsumer.collectByRoutes(routes);
-			// TODO
-			
-			return new ResponseEntity<>(new ArrayList<>(), HttpStatus.OK);
-			
+			FlightSearch flightSearch = new FlightSearch(departure, arrival, departureDateTime, arrivalDateTime);
+			List<String> stopLocations = findInterconnectedLocations(flightSearch);			
+			List<Flight> resultFlights = findFlighs(flightSearch, stopLocations);
+
+			return new ResponseEntity<>(resultFlights, HttpStatus.OK);
+
 		} catch (Exception e) {
 			log.info(e.getMessage());
 			return new ResponseEntity<>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 	
+	private List<String> findInterconnectedLocations(FlightSearch flightSearch) {
+	
+		RouteSearch routeSearch = searchConverter.convert(flightSearch);
+		List<RouteAPI> apiRoutes = routesConsumer.collect();
+		return routesService.findStopLocations(apiRoutes, routeSearch);
+	}
 
-
+	private List<Flight> findFlighs(FlightSearch flightSearch, List<String> stopLocations) {
+		
+		List<Flight> resultFlights = new ArrayList<>();
+		
+		for (String stop : stopLocations) {
+			List<ScheduleAPI> firstLegSchedules = schedulesConsumer.findFirstLeg(flightSearch, stop);
+			List<ScheduleAPI> secondLegSchedules = schedulesConsumer.findSecondLeg(flightSearch, stop);
+			
+			List<Flight> flights = schedulesService.matchConnections(firstLegSchedules, secondLegSchedules);
+			resultFlights.addAll(flights);
+		}
+		
+		return resultFlights;
+	}
 }
