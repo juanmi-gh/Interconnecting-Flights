761a3da724885f5dbf548d1868ce544530bc29ee
juanmi <juancabrera.dev@gmail.com>
2019-07-28 18:13:32 +0200

    Fix missing annotation

--- src/main/java/com/ryanair/jmcr/controller/SearchConverter.java
+++ src/main/java/com/ryanair/jmcr/controller/SearchConverter.java
@@ -3,10 +3,13 @@ package com.ryanair.jmcr.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Service;
+
 import com.ryanair.jmcr.controller.dto.FlightSearch;
 import com.ryanair.jmcr.service.Converter;
 import com.ryanair.jmcr.service.routes.dto.RouteSearch;
 
+@Service
 public class SearchConverter implements Converter<RouteSearch, FlightSearch> {
 
 	@Override
