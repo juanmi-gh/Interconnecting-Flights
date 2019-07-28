0437fdd5e06088039444e9d532ac55b961be3327
juanmi <juancabrera.dev@gmail.com>
2019-07-29 00:13:01 +0200

    Implement filter funtionality

--- src/main/java/com/ryanair/jmcr/service/schedules/ISchedulesService.java
+++ src/main/java/com/ryanair/jmcr/service/schedules/ISchedulesService.java
@@ -2,10 +2,14 @@ package com.ryanair.jmcr.service.schedules;
 
 import java.util.List;
 
+import com.ryanair.jmcr.controller.dto.FlightSearch;
+import com.ryanair.jmcr.model.Schedule;
 import com.ryanair.jmcr.service.dto.Flight;
 import com.ryanair.jmcr.service.schedules.dto.ScheduleAPI;
 
 public interface ISchedulesService {
 
+	List<Flight> filterSchedules(FlightSearch flightSearch, List<Schedule> routeSchedules);
+	
 	List<Flight> matchConnections(List<ScheduleAPI> firstLegSchedules, List<ScheduleAPI> secondLegSchedules);
 }
--- src/main/java/com/ryanair/jmcr/service/schedules/impl/SchedulesService.java
+++ src/main/java/com/ryanair/jmcr/service/schedules/impl/SchedulesService.java
@@ -1,10 +1,19 @@
 package com.ryanair.jmcr.service.schedules.impl;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import com.ryanair.jmcr.controller.dto.FlightSearch;
+import com.ryanair.jmcr.model.FlightInfo;
+import com.ryanair.jmcr.model.Schedule;
 import com.ryanair.jmcr.service.dto.Flight;
+import com.ryanair.jmcr.service.dto.Leg;
 import com.ryanair.jmcr.service.schedules.ISchedulesService;
 import com.ryanair.jmcr.service.schedules.dto.ScheduleAPI;
 
@@ -12,9 +21,63 @@ import com.ryanair.jmcr.service.schedules.dto.ScheduleAPI;
 public class SchedulesService implements ISchedulesService {
 
 	@Override
+	public List<Flight> filterSchedules(FlightSearch flightSearch, List<Schedule> dailySchedules) {
+
+		List<Flight> result = new ArrayList<>();
+
+		List<Schedule> filteredSchedules = filterSchedulesByDate(flightSearch, dailySchedules);
+				
+		LocalTime departureTime = flightSearch.getDepartureDateTime().toLocalTime();
+		LocalTime arrivalTime = flightSearch.getArrivalDateTime().toLocalTime();
+		
+		for(Schedule day : filteredSchedules) {
+			for(FlightInfo info : day.getFlights()) {
+
+				if (info.getDepartureTime().isBefore(departureTime) ||
+					info.getArrivalTime().isAfter(arrivalTime)) {
+					continue;
+				}
+
+				Flight flight = buildFlight(flightSearch, day.getDate(), info);
+				result.add(flight);
+			}
+		}
+
+		return result;
+	}
+
+	private List<Schedule> filterSchedulesByDate(FlightSearch flightSearch, List<Schedule> dailySchedules) {
+		
+		LocalDate departureDate = flightSearch.getDepartureDateTime().toLocalDate();
+		LocalDate arrivalDate = flightSearch.getArrivalDateTime().toLocalDate();
+
+		return dailySchedules.stream()
+				. filter(day -> (departureDate.isBefore(day.getDate()) || departureDate.equals(day.getDate()))
+							&& (arrivalDate.isAfter(day.getDate()) || arrivalDate.equals(day.getDate())))
+				.collect(Collectors.toList());
+	}
+	
+	private Flight buildFlight(FlightSearch flightSearch, LocalDate date, FlightInfo info) {
+		
+		String departure = LocalDateTime.of(date, info.getDepartureTime()).toString();
+		String arrival = LocalDateTime.of(date, info.getArrivalTime()).toString();
+		
+		Leg leg = Leg.builder()
+    				.departureAirport(flightSearch.getDeparture())
+    				.arrivalAirport(flightSearch.getArrival())
+    				.departureDateTime(departure) 
+    				.arrivalDateTime(arrival)
+    				.build();
+		
+		return Flight.builder()
+					.stops(0)
+					.legs(List.of(leg))
+					.build();
+	}
+
+	@Override
 	public List<Flight> matchConnections(List<ScheduleAPI> firstLegSchedules, List<ScheduleAPI> secondLegSchedules) {
-		// TODO Auto-generated method stub
-		return null;
+		return new ArrayList<>();
 	}
 
 }
